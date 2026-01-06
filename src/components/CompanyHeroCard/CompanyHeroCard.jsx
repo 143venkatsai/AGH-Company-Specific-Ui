3301bb0675176efdbdd9355f571516cefafe1a58
143venkatsai <venkatsaipelluru143@gmail.com>
2026-01-06 13:56:24 +0530

    merge changes

--- src/components/CompanyHeroCard/CompanyHeroCard.jsx
+++ src/components/CompanyHeroCard/CompanyHeroCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { CompanyHeroCardWrapper, CompanyLogo } from "./CompanyHeroCard.styles";
 
 const CompanyHeroCard = () => {
-  return <></>;
+  return (
+    <>
+      <CompanyHeroCardWrapper>
+        <CompanyLogo src="https://via.placeholder.com/150" alt="Company Logo" />
+      </CompanyHeroCardWrapper>
+    </>
+  );
 };
 
 export default CompanyHeroCard;
--- src/components/CompanyHeroCard/CompanyHeroCard.styles.js
+++ src/components/CompanyHeroCard/CompanyHeroCard.styles.js
@@ -40,3 +40,43 @@ export const CompanyTagline = styled.p`
   color: #6e6e6e;
   margin: 0;
 `;
+
+export const CompanyInfoBottom = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+`;
+
+export const InfoItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const InfoLogoContainer = styled.div`
+  background-color: #e6f7f1;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100%;
+`;
+
+export const InfoTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const InfoLabel = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  color: #9b9b9b;
+`;
+
+export const InfoLabelItems = styled.div`
+  font-size: 16px;
+  color: #515151;
+  font-weight: 400;
+`;
